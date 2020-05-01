3922e877569fde750fefb7c096475c0173cae67a
Tarthan <61007686+Tarthan@users.noreply.github.com>
2020-05-01 21:53:39 +0800

    New map/completed ready for codes

---
+++ Brief3game REDO/Assets/scripts/Elevator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    public GameObject moveplatform;
+
+    private void OnTriggerStay()
+    {
+        moveplatform.transform.position += moveplatform.transform.up * Time.deltaTime;
+    }
+
+
+
+
+
+
+
+}
---
+++ Brief3game REDO/Assets/scripts/PlayerControllerThatLevelsUp.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ 
+       Player Controller with that moves and turns faster as it gains XP;
+	    Manages moving and rotating the player via the arrow keys.
+		Up/down arrow keys to move forward and backward.
+		Left/right arrow keys to rotate left and right.
+*/
+
+public class PlayerControllerThatLevelsUp : MonoBehaviour
+{
+
+    //The base move and turn speed
+    public float moveSpeed = 5f;
+    public float turnSpeed = 100f;
+    public float jumpHeight = 5f;       // Upward velocity when user presses spacebar.
+
+    //The move and turn speed with the buffs you have from leveling up.   
+    public float currentMoveSpeed;
+    public float currentTurnSpeed;
+
+
+
+    public float xp = 0;	// Amount of XP the player has
+    public float xpForNextLevel = 10;   //Xp needed to level up, the higher the level, the harder it gets. 
+    public int level = 0;   // Level of the player
+
+
+
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        SetXpForNextLevel();
+        SetCurrentMoveSpeed();
+        SetCurrentTurnSpeed();
+    }
+
+
+
+    // To level up you need to collect an amount of xp;
+    // This starts at 10 xp
+    // Each level you gain the xp required gets higher exponentially
+    // The exponential growth is slowed by scaling it by 10%
+
+    void SetXpForNextLevel()
+    {
+        xpForNextLevel = (10f + (level * level * 0.1f));
+        Debug.Log("xpForNextLevel " + xpForNextLevel);
+    }
+
+
+
+    // For each level, the player adds 10% to the move speed 
+    void SetCurrentMoveSpeed()
+    {
+        currentMoveSpeed = this.moveSpeed + (this.moveSpeed * 0.1f * level);
+        Debug.Log("currentMoveSpeed = " + currentMoveSpeed);
+    }
+
+    // For each level, the player adds 10% to the turn speed 
+    void SetCurrentTurnSpeed()
+    {
+        currentTurnSpeed = this.turnSpeed + (this.turnSpeed * (level * 0.1f));
+        Debug.Log("currentTurnSpeed = " + currentTurnSpeed);
+    }
+
+
+    void LevelUp()
+    {
+        xp = 0f;
+        level++;
+        Debug.Log("level" + level);
+        SetXpForNextLevel();
+        SetCurrentMoveSpeed();
+        SetCurrentTurnSpeed();
+
+    }
+
+
+
+
+    //a function to make the player gain the ammount of Xp the you tell it. 
+    public void GainXP(int xpToGain)
+    {
+        xp += xpToGain;
+        Debug.Log("Gained " + xpToGain + " XP, Current Xp = " + xp + ", XP needed to reach next Level = " + xpForNextLevel);
+    }
+
+
+    void Update()
+    {
+        //Test the GainXp function by pressing the x button. 
+        if (Input.GetKeyDown(KeyCode.X) == true) { GainXP(1); }
+
+
+        //LevelUp when the appropriate conditions are met.
+        if (xp >= xpForNextLevel)
+        {
+            LevelUp();
+        }
+
+
+
+
+
+        float translation = Input.GetAxis("Vertical") * moveSpeed;
+        float straffe = Input.GetAxis("Horizontal") * moveSpeed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+        if (Input.GetKey(KeyCode.Space) == true && Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 0.01f)
+        {
+            this.GetComponent<Rigidbody>().velocity += Vector3.up * this.jumpHeight;
+        }
+    }
+}
